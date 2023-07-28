259b49b24a9f9eb3a8388494285a067b465ba3b0
ishitabansal21 <100590854+ishitabansal21@users.noreply.github.com>
2023-07-28 10:38:01 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
